ce8419498ed4ef6d3a9df6d10c7feb138d5275f8
PoojithaKomaravelli <pujipoojitha5@gmail.com>
2021-06-14 09:50:59 -0500

    Implement Swagger documentation

--- src/main/java/com/beachbody/gymservice/GymServiceApplication.java
+++ src/main/java/com/beachbody/gymservice/GymServiceApplication.java
@@ -2,9 +2,13 @@ package com.beachbody.gymservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -12,13 +16,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableWebSecurity
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.beachbody.gymservice"})
 public class GymServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GymServiceApplication.class, args);
 	}
 
